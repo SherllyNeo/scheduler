9672498d6979a270386f539008ae4bcfad3f2a52
Sherlly <SherllyNeo@gmail.com>
2022-04-30 14:47:12 +0100

    misc: cleaning repo

---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+import schedule
+import time
+import pandas as pd
+from log_file import log_files
+from valid_timer import valid_time
+from icecream import ic
+
+ic.enable()
+
+
+def start_thread():
+    log_control = log_files()
+    log_control.check_exist()
+    time.sleep(1)
+    if log_control.check_if_something_is_happening():
+        return
+    else:
+        current_activity = input("what are you doing?\n")
+        log_control.add_log_to_file(current_activity)
+        log_control.refresh_logs()
+        log_control.dirty_to_clean()
+
+while True:
+    time.sleep(1)
+    print("checking")
+
+    if valid_time():
+        print("starting checker")
+        start_thread()
+    else:
+        ic("invalid time: appending no-task to datetime")
+        log_control = log_files()
+        log_control.refresh_logs()
+        log_control.set_inactive()
+        log_control.dirty_to_clean()
